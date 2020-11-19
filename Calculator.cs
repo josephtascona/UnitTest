4d71d5ab421858425b952e3e9b08df99c02b5fcc
Joseph Tascona <joseph.tascona@gmail.com>
2020-11-19 05:01:14 -0500

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UnitTest
+{
+    public class Calculator : IDisposable
+    {
+        public int Addition(int first, int second)
+        {
+            return first + second;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Subtraction(int first, int second)
+        {
+            //this method will never return -ve no. to demonstrate argument validations
+            if (first < second)
+            {
+                throw new ArgumentException($"First number {first} is less than second number {second}");
+            }
+
+            return first - second;
+        }
+    }
+}
